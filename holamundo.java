f58e1da5f0e01246851d00595ca3abce138c27fc
xavilb <alum.xmarcelleonesb@iesalixar.org>
2021-09-27 14:08:18 +0200

    comentarios hechos por xavi

--- holamundo.java
+++ holamundo.java
@@ -1,3 +1,9 @@
+/**
+*Muestra por pantalla la frase "¡Hola mundo !"
+*
+*@author xavilb
+*//
+
 public class HolaMundo {
 	public static void main (String [] args) {
 		System.out.println ("¡Hola Mundo!");
