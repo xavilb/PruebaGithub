b89f433e073f856452abe1d1414c46f28e38b8b5
xavilb <alum.xmarcelleonesb@iesalixar.org>
2021-09-29 10:14:52 +0200

    Colores definidos

--- holamundo.java
+++ holamundo.java
@@ -7,6 +7,10 @@
 public class HolaMundo {
 	public static void main (String [] args) {
 		System.out.println ("¡Hola Mundo!");
+				String naranja = *\033[33m";
+				String azul = "\033[32m";
+				System.out.println("¡Hola mundo!");
+							System.out.println("Aqui estamos haciendo una prueba.");
 	}
 }
 
